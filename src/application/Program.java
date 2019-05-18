1e9fd5334bc32c655cd9dc0cc1a256d24cab9267
HwoarangBrz <ricardo.s.jorge@hotmail.com>
2019-05-18 18:49:21 -0300

    SellerDAO update

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,11 +16,13 @@ public class Program {
 		System.out.println("=== TEST 1: sellerById ===");
 		Seller seller = sellerDao.findById(2);
 		System.out.println(seller);
+		
 		System.out.println("=== TEST 2: sellerByDepartment ===");
 		List<Seller> list = sellerDao.findByDepartment(seller.getDepartment());
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
 		System.out.println("=== TEST 3: sellerAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
@@ -30,7 +32,14 @@ public class Program {
 		System.out.println("=== TEST 4: insert ===");
 		Seller newSeller = new Seller(null, "Greg Two", "greg@gmail.com", new Date(), 4000.0, seller.getDepartment());
 		sellerDao.insert(newSeller);
-		System.out.println("New seller inserted: " + newSeller.getId());
+		System.out.println("New seller inserted");
+		System.out.println(newSeller);
+		
+		System.out.println("=== TEST 5: update ===");
+		newSeller.setEmail("greg2@gmail.com");;
+		sellerDao.update(newSeller);
+		System.out.println("Seller updated");
+		System.out.println(newSeller);
 		
 	}
 
